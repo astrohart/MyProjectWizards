c29ab539521cabee1c88f1797bf2b669dee7f345
Brian Hart <brian.hart@xylogix.net>
2023-09-16 19:47:55 -0600

    Updated and reinstalled NuGet packages in solution

---
+++ MyProjectWizard.Tests/ExampleDialogBoxTests.cs
@@ -0,0 +1,60 @@
+﻿using Core.Config;
+using NUnit.Framework;
+using System;
+using System.Windows.Forms;
+
+namespace MyProjectWizard.Tests
+{
+///  <summary> Provides unit tests for the methods, properties, and events of the <see cref="T:Foo.ExampleDialogBox" /> class. </summary>
+    [TestFixture]
+    public class ExampleDialogBoxTests
+    {
+///  <summary> Initializes the state of this fixture for every unit test session. </summary>
+        [SetUp]
+        public void Initialize()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+
+///  <summary> Checks that the dialog check boxes are set properly when a <see cref="T:Core.Config.Configuration" /> is initialized a certain way. </summary>
+        [Test]
+        public void Test_Configuration_DataBinding_Works()
+        {
+            var config = MakeNewConfiguration.FromScratch()
+                                             .SetCreateDataAccessLayer(true)
+                                             .SetCreateUserInterfaceLayer(
+                                                 false
+                                             );
+
+            Assert.That(config, Is.Not.Null);
+            Assert.That(config.CreateUserInterfaceLayer, Is.False);
+            Assert.That(config.CreateDataAccessLayer, Is.True);
+
+            using (var exampleDialogBox = MakeNewExampleDialogBox.FromScratch())
+            {
+                exampleDialogBox.CurrentConfiguration = config;
+
+                Assert.That(
+                    exampleDialogBox.CurrentConfiguration.CreateDataAccessLayer,
+                    Is.EqualTo(config.CreateDataAccessLayer)
+                );
+                Assert.That(
+                    exampleDialogBox.CurrentConfiguration
+                                    .CreateUserInterfaceLayer,
+                    Is.EqualTo(config.CreateUserInterfaceLayer)
+                );
+
+                // TODO: Confirm that the check boxes selected match the configuration object set up above.
+                exampleDialogBox.ShowDialog();
+
+                Console.WriteLine(
+                    $"exampleDialogBox.CurrentConfiguration.CreateDataAccessLayer = {exampleDialogBox.CurrentConfiguration.CreateDataAccessLayer}"
+                );
+                Console.WriteLine(
+                    $"exampleDialogBox.CurrentConfiguration.CreateUserInterfaceLayer = {exampleDialogBox.CurrentConfiguration.CreateUserInterfaceLayer}"
+                );
+            }
+        }
+    }
+}
---
+++ MyProjectWizard/MakeNewExampleDialog.cs
@@ -0,0 +1,54 @@
+﻿namespace MyProjectWizard
+{
+    /// <summary>
+    /// Creates new instances of objects that implement the
+    /// <see cref="T:MyProjectWizard.IExampleDialogBox" /> interface, and returns
+    /// references to them.
+    /// </summary>
+    [Log(AttributeExclude = true)]
+    public static class MakeNewExampleDialogBox
+    {
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:Foo.IExampleDialogBox" /> interface and returns a
+        /// reference to it.
+        /// </summary>
+        /// <param name="dte">
+        /// (Required.) Reference to an instance of an object that
+        /// implements the <see cref="T:EnvDTE.DTE" /> interface.
+        /// </param>
+        /// <param name="destinationDirectory">
+        /// (Required.) String containing the
+        /// fully-qualified pathname of the directory to which the new project files are to
+        /// be written.
+        /// </param>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:Foo.IExampleDialogBox" /> interface.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// Thrown if the required
+        /// parameter, <paramref name="dte" />, is passed a <see langword="null" /> value.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentException">
+        /// Thrown if the required parameter,
+        /// <paramref name="destinationDirectory" />, is passed a blank or
+        /// <see langword="null" /> string for a value.
+        /// </exception>
+        public static IExampleDialogBox FromScratch(DTE dte,
+            string destinationDirectory)
+            => new ExampleDialogBox(dte, destinationDirectory);
+
+        /// <summary>
+        /// Creates a new instance of an object that implements the
+        /// <see cref="T:MyProjectWizard.IExampleDialogBox" /> interface and returns a
+        /// reference to it.
+        /// </summary>
+        /// <returns>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:MyProjectWizard.IExampleDialogBox" /> interface.
+        /// </returns>
+        public static IExampleDialogBox FromScratch()
+            => new ExampleDialogBox();
+    }
+}
